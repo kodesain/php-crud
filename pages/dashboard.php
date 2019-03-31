8eacbee900820f4849bfbed91e9d4c37a00de300
Romi A Hakim <romi.hakim@eaciit.com>
2019-03-31 08:34:05 +0700

    PHP Simple CRUD Application

---
+++ pages/dashboard.php
@@ -0,0 +1,44 @@
+<h2>PHP Simple CRUD Application</h2>
+<p>Template: Standard Bootstrap with Sidebar.</p>
+<h4>Module:</h4>
+<ol>
+    <li>Dashboard
+        <ul>
+            <li>Just for Static Content</li>
+        </ul>
+    </li>
+    <li>Categories
+        <ul>
+            <li>Simple CRUD</li>
+            <li>Standard Tabel Data</li>
+            <li>Standard Form Submit</li>
+            <li>Form Validation</li>
+        </ul>
+    </li>
+    <li>Products
+        <ul>
+            <li>Standard Tabel Data</li>
+            <li>Standard Form Submit</li>
+            <li>Option Categories</li>
+            <li>Form Validation</li>
+            <li>File Upload</li>
+            <li>File Validation (Only If Not Empty)</li>
+        </ul>
+    </li>
+    <li>Payments
+        <ul>
+            <li>Ajax Tabel Data</li>
+            <li>Ajax Form Request</li>
+            <li>Ajax Form Validation Response</li>
+        </ul>
+    </li>
+    <li>Shipping
+        <ul>
+            <li>Ajax Tabel Data</li>
+            <li>Ajax Form Request</li>
+            <li>Ajax Form Validation Response</li>
+            <li>Ajax File Upload</li>
+            <li>Ajax File Validation (Only If Not Empty)</li>
+        </ul>
+    </li>
+</ol>
